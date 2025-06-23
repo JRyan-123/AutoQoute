84b35de72b5aa44c109cdde3fc14b210378f94d4
JRyan-123 <acostajoseryan4@gmail.com>
2025-06-24 02:10:19 +0800

    >computation for p2p

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const selectService = document.getElementById('selectService');
 const HSwrapper = document.getElementById('HSwrapper');
 const inputDays = document.getElementById('inputDays');
+
+
 //choose on select service option
 selectService.addEventListener('change' , function() {
 	if (this.value === "hs") {
@@ -16,6 +18,7 @@ selectService.addEventListener('change' , function() {
 		inputDays.classList.remove("hidden");
 	}
 });
+
 //option for HS if selected
 const HSoption = document.getElementById('HSoption');
 const inline = document.querySelector('.inline');
@@ -28,3 +31,22 @@ HSoption.addEventListener("change", function() {
 		inline.classList.remove("hidden");
 	}
 });
+
+const km = document.getElementById('km');
+
+const days = document.getElementById('days');
+const div = 15;
+let result = "";
+
+function calculate() {
+	if (selectService.value === "p2p") {
+		const carCharge = 1000 + (days.value * 1000);
+		const driverFee = 500 + (days.value * 500);
+		const gasToll = Math.ceil(km.value / 25) * 500;
+		const xtraFee = Math.max(0, Math.floor((km.value - 75) / 25)) * 250;	
+		result = `Result: ${carCharge + driverFee + gasToll} and Extra fee of ${xtraFee}`;
+
+	}
+
+	document.getElementById('result').innerText = result;
+}
