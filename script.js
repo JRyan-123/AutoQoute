f0fd1c0e0cce7a8e36af97f996b9500b4683a017
JRyan-123 <acostajoseryan4@gmail.com>
2025-06-25 23:11:52 +0800

    finalize p2p and hg computation

--- script.js
+++ script.js
@@ -43,22 +43,33 @@ const days = document.getElementById('days');
 const div = 15;
 let result = "";
 
+
 function calculate() {
+	if (!km.value || km.value === "0") {
+		alert("Invalid input: must be greater than 0.");
+		return; // Stop the function
+	}
 	if (selectService.value === "p2p") {
+		let xtraFee = 0;
 		const carCharge = 1000 + (days.value * 1000);
 		const driverFee = 500 + (days.value * 500);
 		const gasToll = Math.ceil(km.value / 25) * 500;
-		const xtraFee = Math.max(0, Math.floor((km.value - 75) / 25)) * 250;	
-		result = `Result: ${carCharge + driverFee + gasToll} and Extra fee of ${xtraFee}`;
+		 xtraFee += (Math.floor((km.value)/100)*500);
+		 xtraFee += (Math.ceil((km.value-63)/25)*250);
+
+		
+		result = `Basepay: ${carCharge + driverFee + gasToll} and Extra fee of ${xtraFee}`;
 
 	}
 
 	else if (selectService.value === "hs") {
-
+		  
 	}
 	else{
-		const totalCharge = Math.max(1000, 500 + (km.value * 50));
-		result = `Result: ${totalCharge} and extra 500 for helper`;
+		let xtraFee = 0;
+		xtraFee += (Math.ceil((km.value-5)/8)*50);
+		totalCharge = 500 + ((km.value/100) * 3500);
+		result = `TotalFee: ${Math.max(1000,totalCharge + xtraFee)} and extra 500 for helper` ;
 	}
 	document.getElementById('result').innerText = result;
 }
